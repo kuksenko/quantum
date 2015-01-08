b25ea7fd10878798bf254e68db05a2ba6d92d4dc
Sergey Kuksenko <skuksenko@gmail.com>
2015-01-08 20:04:22 +0300

    updated

--- src/main/java/quantum/demo1/DoubleSumAverageTime.java
+++ src/main/java/quantum/demo1/DoubleSumAverageTime.java
@@ -2,12 +2,14 @@ package quantum.demo1;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 import quantum.util.Utils;
 
 import java.util.concurrent.TimeUnit;
@@ -22,7 +24,9 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Thread)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Mode.AverageTime)
-public class DoubleSumStep2 {
+@Warmup(iterations = 5, time = 2)
+@Measurement(iterations = 5, time = 2)
+public class DoubleSumAverageTime {
 
     public static final int SIZE = 2048;
 
@@ -48,7 +52,7 @@ public class DoubleSumStep2 {
     public double testManualUnroll() {
         double sum = 0.0;
         for (int i = 0; i < array.length; i += 4) {
-            sum += (array[i] + array[i + 1] + array[i + 2] + array[i + 3]);
+            sum += array[i] + array[i + 1] + array[i + 2] + array[i + 3];
         }
         return sum;
     }
--- src/main/java/quantum/demo1/DoubleSumThroughput.java
+++ src/main/java/quantum/demo1/DoubleSumThroughput.java
@@ -1,10 +1,17 @@
 package quantum.demo1;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.CompilerControl;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 import quantum.util.Utils;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Quantum Performance Effects (demo)
@@ -14,7 +21,10 @@ import quantum.util.Utils;
  * @author Sergey Kuksenko
  */
 @State(Scope.Thread)
-public class DoubleSumStep4 {
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 5, time = 2)
+@Measurement(iterations = 5, time = 2)
+public class DoubleSumThroughput {
 
     public static final int SIZE = 2048;
 
@@ -26,6 +36,7 @@ public class DoubleSumStep4 {
     }
 
     @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public double test1() {
         double sum = 0.0;
         for (int i = 0; i < array.length; i++) {
@@ -35,10 +46,11 @@ public class DoubleSumStep4 {
     }
 
     @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public double testManualUnroll() {
         double sum = 0.0;
         for (int i = 0; i < array.length; i += 4) {
-            sum += (array[i] + array[i + 1] + array[i + 2] + array[i + 3]);
+            sum += array[i] + array[i + 1] + array[i + 2] + array[i + 3];
         }
         return sum;
     }
@@ -67,6 +79,29 @@ public class DoubleSumStep4 {
             sum3 += array[i + 3];
         }
         return (sum0 + sum1) + (sum2 + sum3);
+    }
+
+    @Benchmark
+    public double test8() {
+        double sum0 = 0.0;
+        double sum1 = 0.0;
+        double sum2 = 0.0;
+        double sum3 = 0.0;
+        double sum4 = 0.0;
+        double sum5 = 0.0;
+        double sum6 = 0.0;
+        double sum7 = 0.0;
+        for (int i = 0; i < array.length; i += 8) {
+            sum0 += array[i];
+            sum1 += array[i + 1];
+            sum2 += array[i + 2];
+            sum3 += array[i + 3];
+            sum4 += array[i + 4];
+            sum5 += array[i + 5];
+            sum6 += array[i + 6];
+            sum7 += array[i + 7];
+        }
+        return ((sum0 + sum1) + (sum2 + sum3)) + ((sum4 + sum5) + (sum6 + sum7));
     }
 
 }
